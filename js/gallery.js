e4863d0c646f5248328b949348193bf35fefb229
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-05-25 00:36:37 +0200

    Formatting JS

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,7 @@
 var galleryItemCount = 6;
 
 for (let galleryItemIndex = 0; galleryItemIndex < galleryItemCount; galleryItemIndex++) {
-    
+
     // using let here is important because otherwise the variable popup (var popup = ...;) 
     // will be overwritten each time the for loop executes meaning that popup will be
     // equal to the last popup item (galleryItemIndex = 5)
@@ -9,12 +9,12 @@ for (let galleryItemIndex = 0; galleryItemIndex < galleryItemCount; galleryItemI
     let popup = document.getElementById("popup-" + galleryItemIndex);
     let popupExit = popup.querySelector(".popup-exit");
 
-    popupExit.addEventListener("click", function(event) {
+    popupExit.addEventListener("click", function (event) {
         popup.classList.remove("show");
         scrollStatus("start")
     });
 
-    popup.addEventListener("click", function(event) {
+    popup.addEventListener("click", function (event) {
         if (event.target == popup) {
             popup.classList.remove("show");
             scrollStatus("start")
@@ -22,12 +22,12 @@ for (let galleryItemIndex = 0; galleryItemIndex < galleryItemCount; galleryItemI
     });
 
     let galleryItem = document.getElementById("gallery-item-" + galleryItemIndex);
-    galleryItem.addEventListener("click", function(event) {
+    galleryItem.addEventListener("click", function (event) {
         popup.classList.add("show");
         scrollStatus("stop")
     });
 
-    document.body.addEventListener("keydown", function(event) {
+    document.body.addEventListener("keydown", function (event) {
         if (event.key === "Escape") {
             popup.classList.remove("show");
             scrollStatus("start")
@@ -39,10 +39,10 @@ for (let galleryItemIndex = 0; galleryItemIndex < galleryItemCount; galleryItemI
 so if the modal is open, it's not possible to scroll,
 but once the modal is closed, you can scroll again */
 function scrollStatus(status) {
-    if (status==="stop") {
+    if (status === "stop") {
         document.body.classList.add("stop-scrolling");
     }
-    else if (status==="start") {
+    else if (status === "start") {
         document.body.classList.remove("stop-scrolling");
     }
     else {
